f9f05af28abb79cd654fc25dd816cce35e0b38dd
Resvith <116066494+Resvith@users.noreply.github.com>
2024-05-04 20:17:08 +0200

    Add primitive NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
-  <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://picsum.photos/id/237/800/600" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/id/257/800/600" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/id/127/800/600" class="d-block w-100" alt="...">
-    </div>
+  <div id="app">
+    <the-nav></the-nav>
+    <router-view></router-view>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 </template>
 
 <script>
@@ -27,9 +10,13 @@
 export default {
   name: 'App',
   components: {
+    TheNav
   }
 }
+
+import TheNav from './components/TheNav.vue'
 </script>
+
 
 <style>
 #app {
---
+++ src/components/TheNav.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">UdemyClone</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Courses Category
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li class="dropdown-submenu">
+                <a class="dropdown-item dropdown-toggle" href="#">Web Development</a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">HTML</a>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a class="dropdown-item dropdown-toggle" href="#">CSS</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Sass</a></li>
+                      <li><a class="dropdown-item" href="#">Less</a></li>
+                      <li><a class="dropdown-item" href="#">Bootstrap</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a class="dropdown-item dropdown-toggle" href="#">JS</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">JQuery</a></li>
+                      <li><a class="dropdown-item" href="#">React</a></li>
+                      <li><a class="dropdown-item" href="#">Angular</a></li>
+                      <li><a class="dropdown-item" href="#">Vue.js</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a class="dropdown-item" href="#">Mobile Development</a></li>
+              <li><a class="dropdown-item" href="#">AI</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex my-2 my-md-0 ms-auto d-none d-md-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <button class="btn btn-outline-primary ms-2 d-lg-none" type="button">Log In</button>
+        <button class="btn btn-primary ms-2 d-lg-none" type="button">Register</button>
+      </div>
+      <button class="btn btn-outline-primary ms-auto d-none d-lg-inline-block" type="button">Log In</button>
+      <button class="btn btn-primary ms-2 d-none d-lg-inline-block" type="button">Register</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNav'
+}
+</script>
+
+<style>
+/* Styles to make submenu is same line above */
+.dropdown-submenu {
+  position: relative;
+}
+
+.dropdown-submenu:hover > .dropdown-menu {
+  display: block;
+}
+
+.dropdown-submenu .dropdown-menu {
+  top: 0;
+  left: 100%;
+  margin-top: -5px;
+}
+</style>
